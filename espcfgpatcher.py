d903a2b4307070916b744a3a4cf6fd202aeed214
Steve Rodgers <steve@rodgers619.com>
2015-02-02 09:41:06 -0800

    Add GPL header and comments

--- espcfgpatcher.py
+++ espcfgpatcher.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+#
+# ESP8266 ROM Patching Utility
+# https://github.com/hwstar/espconfigpatcher
+#
+# Copyright (C) 2015 Stephen Rodgers
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 __author__ = 'srodgers'
 
 import struct
@@ -13,6 +31,7 @@ ESP_IMAGE_MAGIC = 0xe9
 # Initial state for the checksum routine
 ESP_CHECKSUM_MAGIC = 0xef
 
+# Signature to search for in image
 SIGNATURE = "ESP8266HWSTARSR"
 
 class ESPFirmwareImage:
